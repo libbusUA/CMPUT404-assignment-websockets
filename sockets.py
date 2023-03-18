0e9a117b71fd00317851d57449b0c210d974fc8f
libbusUA <libbus@ualberta.ca>
2023-03-18 14:22:22 -0600

    Implemented hello function from previous assignment

--- sockets.py
+++ sockets.py
@@ -89,7 +89,7 @@ myWorld.add_set_listener( set_listener )
 @app.route('/')
 def hello():
     '''Return something coherent here.. perhaps redirect to /static/index.html '''
-    return None
+    return flask.redirect('static/index.html')
 
 def read_ws(ws,client):
     '''A greenlet function that reads from the websocket and updates the world'''
